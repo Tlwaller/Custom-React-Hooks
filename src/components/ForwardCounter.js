de84e48b979f9f21ae129839100c28cc15b560bf
Trey Waller <tlwaller99@gmail.com>
2022-02-22 08:40:03 -0600

    replaced separate counters with one custom hook

---
+++ src/components/ForwardCounter.js
@@ -0,0 +1,10 @@
+import Card from "./Card";
+import useCounter from "../Hooks/use-counter";
+
+const ForwardCounter = () => {
+  const counter = useCounter();
+
+  return <Card>{counter}</Card>;
+};
+
+export default ForwardCounter;
